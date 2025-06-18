13159408f209fa52436501f0761af9a15bb02099
M P Darshan <127178559+DarshanD4@users.noreply.github.com>
2025-06-18 09:12:18 +0530

    Update main.c

--- 12-06-25/main.c
+++ 12-06-25/main.c
@@ -24,3 +24,56 @@
 
 // 	return 0;
 // }
+// 	char a;
+// 	printf("Find whether alphabet,number or special character \n");
+// 	printf("Enter the character : ");
+// 	scanf("%c",&a);
+// 	if(a >= 'a' & a <= 'z' || a >= 'A' & a <= 'Z' ) {
+// 		printf("Alphabet");
+// 	}
+// 	else if (a >= '0' & a <= '9') {
+// 		printf("Number");
+// 	}
+// 	else {
+// 		printf("Special character");
+// 	}
+
+// 	4.
+// 	int a;
+// 	printf("Check Leap year or not \n");
+// 	printf("Enter the Year : ");
+// 	scanf("%d",&a);
+// 	if(a%4 == 0 & a%100 != 0) {
+// 		printf("Leap Year");
+// 	}
+// 	else {
+// 		printf("Not a Leap Year");
+// 	}
+
+// 	5.
+// 	int a;
+// 	printf("Map numbers(1-7) to week names \n");
+// 	printf("Enter a number : ");
+// 	scanf("%d",&a);
+// 	if(a == 1) {
+// 		printf("Monday");
+// 	}
+// 	else if(a == 2) {
+// 		printf("Tuesday");
+// 	}
+// 	else if(a == 3) {
+// 		printf("Wednesday");
+// 	}
+// 	else if(a == 4) {
+// 		printf("Thursday");
+// 	}
+// 	else if(a == 5) {
+// 		printf("Friday");
+// 	}
+// 	else if(a == 6) {
+// 		printf("Saturday");
+// 	}
+// 	else if(a == 7) {
+// 		printf("Sunday");
+// 	}
+
